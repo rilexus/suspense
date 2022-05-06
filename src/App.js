7a3b100beb2f356806f465a7481b1af7d5516198
stanislav.panchenko <stanislav.panchenko@ubermetrics-technologies.com>
2022-05-06 15:57:54 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+
+const sleep = async (time = 200) => new Promise((res) => setTimeout(res, time))
+
+
+const lazy = (factory) => {
+  let Component = () => null;
+  let status = 'pending';
+
+  // load JS module right away, when lazy is called
+  let promise = factory()
+      // wait for the JS module to be resolved
+      .then((module) => {
+        status = 'resolved';
+        // get the default export from a file
+        Component = module.default
+      })
+      .catch((e) => {
+        // handle error
+        status = 'error'
+      });
+
+  // return a React component function
+  return (props) => {
+    useEffect(() => {
+      // if still pending, throw to MySuspense to be catched
+      if (status === "pending"){
+        throw promise;
+      }
+    }, []);
+
+    return <Component {...props}/>
+  }
+};
+
+class MySuspense extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoading: false };
+
+  }
+
+  componentDidCatch(error, errorInfo) {
+    if (error.then){
+      this.setState({
+        // show fallback
+        isLoading: true
+      })
+      error.then(() => {
+        // promise is resolved, render children
+        this.setState({
+          isLoading: false
+        })
+      }).catch(e => {
+        // handle error
+      })
+    }
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return this.props.fallback
+    }
+    return this.props.children;
+  }
+}
+
+
+const LazyComponent = lazy(async () => {
+  await sleep(2000); // simulate a long request
+  return import('./LazyComponent')
+})
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MySuspense fallback={<h1>Loading...</h1>}>
+      <LazyComponent/>
+    </MySuspense>
   );
 }
 
---
+++ src/LazyComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LazyComponent = () => {
+    return (
+        <div style={{
+            fontSize: '10rem'
+        }}>
+            <div>🖕</div>
+        </div>
+    );
+};
+
+export default LazyComponent;
